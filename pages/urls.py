5383f7c4b420d70c866dfac98bf83e1c46ec5199
Sidharth Jain <sidharthjainp9@gmail.com>
2024-06-19 11:40:12 +0530

    dynamic links homepage menubar set

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    
+    #homepage views menu items
+    path('about',views.about,name='about'),
+    path('services',views.services,name='services'),
+    path('contact',views.contact,name='contact'),
 ]
