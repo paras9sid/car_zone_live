edabe75af03b7f9cdc389039d5ba331610abbb3f
Sidharth Jain <sidharthjainp9@gmail.com>
2024-06-19 14:34:22 +0530

    admin panel team customized fields

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Teams
+from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(Teams)
+
+#ADmin panel customization
+
+class TeamAdmin(admin.ModelAdmin):
+    
+    #images to render in admin panel lists
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius:30px;" />'.format(object.photo.url))
+    
+    #rename menu thumbnail ->  to -> image
+    thumbnail.short_description="Image"
+    
+    list_display=('id','thumbnail','firstName','lastName','designation','created_at',)
+    list_display_links=('id','thumbnail','firstName',)
+    search_fields=('firstName','lastName','designation',)
+    list_filter=('designation',)
+    
+
+
+admin.site.register(Teams,TeamAdmin)
